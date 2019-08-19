c496da3994f2c48f338b4c3c332d0db1567f86b5
ezjinfe <felix.f.jin@ericsson.com>
2019-08-19 18:19:38 +0800

    Added plotting spectrogram and reloading i/q

--- radioDSP/radiosignal.py
+++ radioDSP/radiosignal.py
@@ -5,21 +5,31 @@ from matplotlib import pyplot as plt
 
 
 class RadioSignal():
-    def __init__(self, bit_width, fs):
+    def __init__(self, bit_width, fs, i = None, q = None):
         '''
         :param bit_width: iq valid bit width
         :param fs: Mhz
         '''
         self.bit_width = bit_width
         self.fs = fs
+        self.i = i
+        self.q = q
 
-    def make_wave(self,i, q):
+    def reload(self,i,q):
+        '''
+        reload IQ data
+        :param i:
+        :param q:
+        :return:
+        '''
+        self.i = i
+        self.q = q
+
+    def make_wave(self):
         '''
         calculate time domain power with unit DB
-        :param i: narray
-        :param q: narray
         '''
-        self.iq = i + 1j * q
+        self.iq = self.i + 1j * self.q
         if np.isreal(self.iq).all():
             full_scale_rms = (2 ** (self.bit_width - 1) - 1) / np.sqrt(2)
         else:
@@ -28,16 +38,14 @@ class RadioSignal():
         self.signal_power = np.abs(self.iq)
         self.power_db = 20 * np.log10(np.sqrt(self.signal_power)/full_scale_rms+ sys.float_info.min)
 
-    def make_spectrum(self, i, q, window_type = "blackman", is_remove_dc = False, is_full_scale_spectrum = False):
+    def make_spectrum(self, window_type = "blackman", is_remove_dc = False, is_full_scale_spectrum = False):
         '''
         make spectrum from i,q narray
-        :param i: narray
-        :param q: narray
         :param window_type: "blackman", "hamming", "hanning"
         :param is_remove_dc: remove DC flag, True/False
         :param is_full_scale_spectrum: full/half spectrum flag, True/False
         '''
-        self.iq = i + 1j * q
+        self.iq = self.i + 1j * self.q
         if np.isreal(self.iq).all():
             full_scale_rms = (2 ** (self.bit_width - 1) - 1) / np.sqrt(2)
         else:
@@ -85,6 +93,18 @@ class RadioSignal():
         plt.plot(self.power_db)
         plt.xlabel('Time domain(T = 1/{}MHz)'.format(self.fs))
         plt.ylabel('Power(dB)')
+        plt.show()
+
+
+    def plot_spectrogram(self):
+        '''
+        plot spectrogram(Freq-Time)
+        :return:
+        '''
+        plt.figure(figsize=(6, 5))
+        plt.specgram(self.power_db, Fs = self.fs)
+        plt.xlabel('Time domain(T = 1/{})'.format(self.fs))
+        plt.ylabel('Freq(MHz)')
         plt.show()
 
 
